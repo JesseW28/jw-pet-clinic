af8fee7b97c18996413a41c68145232f281c018f
Jesse Wouters <jesse.wouters@jcore.com>
2021-03-15 09:55:26 +0100

    initial commit

---
+++ src/main/java/com/jw/jwpetclinic/JwPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.jw.jwpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jw/jwpetclinic/JwPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jw.jwpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
