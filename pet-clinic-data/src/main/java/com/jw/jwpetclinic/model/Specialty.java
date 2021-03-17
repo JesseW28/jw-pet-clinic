4fa18b53b4c48a1dd9f293d31f600b44a474fc4e
Jesse Wouters <jesse.wouters@jcore.com>
2021-03-17 12:55:42 +0100

    Edited entities

---
+++ pet-clinic-data/src/main/java/com/jw/jwpetclinic/model/Specialty.java
@@ -0,0 +1,18 @@
+package com.jw.jwpetclinic.model;
+
+public class Specialty extends BaseEntity {
+
+    private String description;
+
+    public Specialty(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- pet-clinic-data/src/main/java/com/jw/jwpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/jw/jwpetclinic/model/Vet.java
@@ -1,8 +1,20 @@
 package com.jw.jwpetclinic.model;
 
-public class Vet extends Person{
+import java.util.Set;
+
+public class Vet extends Person {
+
+    private Set<Specialty> specialties;
 
     public Vet(String firstName, String lastName) {
         super(firstName, lastName);
+    }
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
     }
 }
